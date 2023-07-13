c12faf365611bec5e30495b48a33ae1e11e4799b
BradleyThomas31 <112415546+BradleyThomas31@users.noreply.github.com>
2023-07-13 17:37:15 -0400

    Game Mechanics Complete

--- Deck.java
+++ Deck.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
-
 import java.util.Random;
 
 public class Deck {
 
-	private Card[] cards;
-
+	private Card[] deckCards;
+	private Card[] playerHand;
+	private Card[] computerHand;
+	private int numPlayerCards;
+	
 	public Deck() {
-		cards = new Card[52];
+		deckCards = new Card[52];
+		playerHand = new Card[52];
+		computerHand = new Card[52];
 		createDeck();
 		shuffleDeck();
 		chooseGame();
@@ -20,7 +24,7 @@ public class Deck {
 		int deckIndex = 0;
 		for (int i = 0; i < suits.length; i++) {
 			for (int j = 0; j < ranks.length; j++) {
-				cards[deckIndex] = new Card(ranks[j], suits[i]);
+				deckCards[deckIndex] = new Card(ranks[j], suits[i]);
 				deckIndex++;
 			}
 		}
@@ -28,133 +32,148 @@ public class Deck {
 
 	public void shuffleDeck() {
 		Random rand = new Random();
-		for (int i = 0; i < cards.length; i++) {
-			int j = rand.nextInt(cards.length);
-			Card temp = cards[i];
-			cards[i] = cards[j];
-			cards[j] = temp;
+		for (int i = 0; i < deckCards.length; i++) {
+			int j = rand.nextInt(deckCards.length);
+			Card temp = deckCards[i];
+			deckCards[i] = deckCards[j];
+			deckCards[j] = temp;
 		}
 	}
 
 	public void chooseGame() {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("What game would you like to play?\nBlackjack (A)\nOther (B)");
+		System.out.println("What game would you like to play?\nBlackjack (A)\nWar (B)\nOther (C)");
 		String input = scan.nextLine();
-		if (input.equals("A")) {
+		if (input.equals("A"))
 			playBlackJack();
-		} else {
+		else if (input.equals("B"))
+			playWar();
+		else
 			System.out.println("error");
+	}
+
+	public void playWar() {
+		for (int i = 0; i < 26; i++) {
+			playerHand[i] = takeCard();
+			computerHand[i] = takeCard();
+		}
+		int playerCardNum = 26;
+		int computerCardNum = 26;
+		while (playerCardNum > 0 && computerCardNum > 0) {
+			playerCardNum--;
 		}
 	}
 
 	public void playBlackJack() {
-		Card[] playerHand = { takeCard(), takeCard() };
-		Card[] dealerHand = { takeCard(), takeCard() };
+		playerHand[0] = takeCard();
+		playerHand[1] = takeCard();
+		numPlayerCards += 2;
+		computerHand[0] = takeCard();
+		computerHand[1] = takeCard();
 
 		Scanner sc = new Scanner(System.in);
 		boolean endGame = false;
-		
-		while(endGame == false) {
+
+		while (endGame == false && addCardValuesBJ(playerHand) < 21) {
 			System.out.println("-------------------------------");
+			
 			System.out.println("\nYour cards:");
-			printHand(playerHand);
-			
+			printHand(playerHand, numPlayerCards);
 			System.out.println("\nDealer's cards:");
-			for (int i = 0; i< dealerHand.length-1; i++) {
-				System.out.println(dealerHand[i].getRank() + " of " + dealerHand[i].getSuit());
-			}
-			
+			printHand(computerHand, 2);
+
 			System.out.println("\n\nHit Me! (A) \nHold (B)");
 			String input = sc.nextLine();
-			
+
 			if (input.equals("A")) {
-				Card[] newPlayerHand = new Card[playerHand.length + 1];
-				for (int i = 0; i < playerHand.length; i++) {
-					newPlayerHand[i] = playerHand[i];
-				}
-				newPlayerHand[playerHand.length] = takeCard();
-				playerHand = newPlayerHand;
-			}
-			else
-				endGame = true;	
+				numPlayerCards++;
+				playerHand[numPlayerCards - 1] = takeCard();
+				printHand(playerHand, numPlayerCards);
+			} else
+				endGame = true;
 		}
-		
+
 		System.out.println("-------------------------------");
 		System.out.println("\nYour Hand:");
-		printHand(playerHand);
+		printHand(playerHand, numPlayerCards);
 		System.out.println("\nDealer's Hand:");
-		printHand(dealerHand);
-		
-		//add them up
+		printHand(computerHand, 2);
+
+		// add them up
 		int playerValue = addCardValuesBJ(playerHand);
-		int dealerValue = addCardValuesBJ(dealerHand);
-		
-		if(playerValue > 21)
+		int dealerValue = addCardValuesBJ(computerHand);
+
+		if (playerValue > 21)
 			System.out.println("\nBust!");
-		else if(playerValue > dealerValue)
+		else if (playerValue > dealerValue)
 			System.out.println("\nYou Win!");
 		else
 			System.out.println("\nYou Lose");
+		
+		System.out.println(playerValue);
+		System.out.println(dealerValue);
 	}
 
 	public int addCardValuesBJ(Card myCards[]) {
 		int handValue = 0;
-		for(int i = 0; i < myCards.length; i++) {
-			switch(myCards[i].getRank()) {
-			case "Ace":
-				handValue += 1;
-				break;
-			case "Two" :
-				handValue += 2;
-				break;
-			case "Three" :
-				handValue += 3;
-				break;
-			case "Four" :
-				handValue += 4;
-				break;
-			case "Five" :
-				handValue += 5;
-				break;
-			case "Six" :
-				handValue += 6;
-				break;
-			case "Seven" :
-				handValue += 7;
-				break;
-			case "Eight" :
-				handValue += 8;
-				break;
-			case "Nine" :
-				handValue += 9;
-				break;
-			case "Ten" :
-				handValue += 10;
-				break;
-			case "Jack" :
-			case "King" :
-			case "Queen" :
-				handValue += 11;
-				break;
-			default :
-				handValue -= 1000;
-			}
+		for (int i = 0; i < myCards.length; i++) {
+			if (myCards[i] != null)
+				switch (myCards[i].getRank()) {
+				case "Ace":
+					handValue += 1;
+					break;
+				case "Two":
+					handValue += 2;
+					break;
+				case "Three":
+					handValue += 3;
+					break;
+				case "Four":
+					handValue += 4;
+					break;
+				case "Five":
+					handValue += 5;
+					break;
+				case "Six":
+					handValue += 6;
+					break;
+				case "Seven":
+					handValue += 7;
+					break;
+				case "Eight":
+					handValue += 8;
+					break;
+				case "Nine":
+					handValue += 9;
+					break;
+				case "Ten":
+					handValue += 10;
+					break;
+				case "Jack":
+				case "King":
+				case "Queen":
+					handValue += 11;
+					break;
+				default:
+					handValue -= 1000;
+				}
 		}
 		return handValue;
 	}
-	
-	public void printHand(Card[] myHand) {
-		for (int i = 0; i < myHand.length; i++) {
-			System.out.println(myHand[i].getRank() + " of " + myHand[i].getSuit());
+
+	public void printHand(Card[] myHand, int num) {
+		for (int i = 0; i < num; i++) {
+			if (myHand[i] != null)
+				System.out.println(myHand[i].getRank() + " of " + myHand[i].getSuit());
 		}
 	}
-	
+
 	public Card takeCard() {
-		Card myCard = cards[0];
-		for (int i = 1; i < cards.length; i++) {
-			cards[i - 1] = cards[i];
+		Card myCard = deckCards[0];
+		for (int i = 1; i < deckCards.length; i++) {
+			deckCards[i - 1] = deckCards[i];
 		}
-		cards[cards.length - 1] = null;
+		deckCards[deckCards.length - 1] = null;
 		return myCard;
 	}
 
